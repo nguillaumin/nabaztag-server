4ba5330763616dc198373fd3248caf61ae5c63e5
nguillaumin <nicolas+github@guillaumin.me>
2011-07-31 10:40:51 +1000

    Added SQL to initialise 'hardware' table.

---
+++ net.violet.platform/src/main/sql/init-hardware.sql
@@ -0,0 +1,12 @@
+DELETE FROM `hardware`;
+
+INSERT INTO `hardware` VALUES (0,NULL,"None",NULL);
+INSERT INTO `hardware` VALUES (3,"v1","Nabaztag",NULL);
+INSERT INTO `hardware` VALUES (4,"v2","Nabaztag",NULL);
+INSERT INTO `hardware` VALUES (5,"book","RFID",NULL);
+INSERT INTO `hardware` VALUES (6,NULL,"Mirror",NULL);
+INSERT INTO `hardware` VALUES (7,NULL,"Daldal",NULL);
+INSERT INTO `hardware` VALUES (8,"ztamp", "RFID",NULL);
+INSERT INTO `hardware` VALUES (9,"nanoztag", "RFID",NULL);
+INSERT INTO `hardware` VALUES (10,"other", "RFID",NULL);
+
